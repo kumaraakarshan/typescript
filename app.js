ab55a86680cb2ab79bd25ce1fbea3c289e0296f7
kumaraakarshan <aakarshan0005@gmail.com>
2023-08-27 11:07:09 +0530

    type script configured

---
+++ app.js
@@ -0,0 +1,20 @@
+var num1Element = document.getElementById('num1');
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
+function add(num1, num2) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + '' + num2;
+    }
+    return +num1 + +num2;
+}
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2);
+    var stringResult = add(num1, num2);
+    console.log(stringResult);
+    console.log(result);
+});
