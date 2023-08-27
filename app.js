fa90b57911bafeba8338b4476a62eef3c0d4ca60
kumaraakarshan <aakarshan0005@gmail.com>
2023-08-27 13:40:31 +0530

    typescript promises

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
+var numResults = [];
+var stringResults = [];
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -10,11 +12,24 @@ function add(num1, num2) {
     }
     return +num1 + +num2;
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
+    numResults.push(result);
     var stringResult = add(num1, num2);
-    console.log(stringResult);
+    stringResults.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, stringResults);
+});
+var myPromise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('it worked');
+    }, 1000);
+});
+myPromise.then(function (result) {
     console.log(result);
 });
--- app.ts
+++ app.ts
@@ -3,7 +3,16 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2')as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
-function add (num1:number|string, num2:number|string)
+const numResults : Array<number> =[];
+const stringResults : string[]=[];
+type numOrString =number |string;
+type Result = { val:number; timestamp:Date}
+
+interface ResultObj{
+    val:number;
+    timestamp:Date;
+}
+function add (num1:numOrString, num2:numOrString)
 {
     if(typeof num1==='number' && typeof num2==='number')
     {
@@ -15,18 +24,33 @@ function add (num1:number|string, num2:number|string)
     }
     return +num1 + +num2;
 }
+
+function printResult(resultObj:ResultObj)
+{
+console.log(resultObj.val);
+
+}
 buttonElement?.addEventListener('click',()=>{
     const num1 =num1Element.value;
     const num2 =num2Element.value;
     const result= add(+num1,+num2);
-
+numResults.push(result as number);
     const stringResult =add(num1,num2);
-    console.log(stringResult);
-    
-    console.log(result);
-    
+    stringResults.push(stringResult as string);
+ 
+    printResult({val: result as number,timestamp: new Date()});
+console.log(numResults,stringResults);
 
 
 })
 
+const myPromise = new Promise ((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve ('it worked');
+    },1000)
+});
 
+myPromise.then((result)=>{
+    console.log(result);
+    
+})
